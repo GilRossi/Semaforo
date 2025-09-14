e6136d5a33a6ac8c5a2aaf23eabc10ef9d965e58
Gil Rossi <gil.rossi.aguiar@hotmail.com>
2025-09-14 19:55:20 -0300

    First Commit - blank project

--- src/main/java/Semaforo.java
+++ src/main/java/Semaforo.java
@@ -1,24 +1,5 @@
 public class Semaforo {
     public static void main(String[] args) {
-        while(true){
-            System.out.println("VERDE");
-            try{
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("AMARELO");
-            try{
-                Thread.sleep(1500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("VERMELHO");
-            try{
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        
     }
 }
